4a969417fc0909b6b92156204fdc5e8022e7ae7b
mmat16 <barabany.tarakany@yandex.ru>
2022-08-01 15:29:56 +0300

    lib

--- clients/telegram/telegram.go
+++ clients/telegram/telegram.go
@@ -1,6 +1,11 @@
 package telegram
 
-import "net/http"
+import (
+	"net/http"
+	"net/url"
+	"path"
+	"strconv"
+)
 
 type Client struct {
 	host     string
@@ -20,8 +25,32 @@ func newBasePath(token string) string {
 	return "bot" + token
 }
 
-func (c *Client) Updates() {
+func (c *Client) Updates(offset int, limit int) ([]Update, error) {
+	q := url.Values{}
+	q.Add("offset", strconv.Itoa(offset))
+	q.Add("limit", strconv.Itoa(limit))
 
+}
+
+func (c *Client) doRequest(method string, query url.Values) ([]byte, error) {
+	const errMsg = "can't do request"
+
+	u := url.URL{
+		Scheme: "https",
+		Host:   c.host,
+		Path:   path.Join(c.basePath, method),
+	}
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, e.Wr //e.Wrap(errMsg, err)
+	}
+	req.URL.RawQuery = query.Encode()
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, e.Wrap(errMsg, err)
+	}
 }
 
 func (c *Client) SendMessage() {
