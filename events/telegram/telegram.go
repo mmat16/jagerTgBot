07b8ae6ce56b83e2f1eb03c904da0e1cdb97cf87
mmat16 <barabany.tarakany@yandex.ru>
2022-08-01 17:48:57 +0300

    add Fetcher

--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -1,11 +1,75 @@
 package telegram
 
-import "jagerTgBot/clients/telegram"
+import (
+	"jagerTgBot/clients/telegram"
+	"jagerTgBot/events"
+	"jagerTgBot/lib/e"
+	"jagerTgBot/storage"
+)
 
 type Processor struct {
-	tg     *telegram.Client
-	offset int
-	// storage
+	tg      *telegram.Client
+	offset  int
+	storage storage.Storage
 }
 
-func New(client *telegram.Client, storage)
+type Meta struct {
+	ChatId   int
+	Username string
+}
+
+func New(client *telegram.Client, storage storage.Storage) *Processor {
+	return &Processor{
+		tg:      client,
+		storage: storage,
+	}
+}
+
+func (p *Processor) Fetch(limit int) ([]events.Event, error) {
+	updates, err := p.tg.Updates(p.offset, limit)
+	if err != nil {
+		return nil, e.WrapIfErr("can't get events", err)
+	}
+
+	if len(updates) == 0 {
+		return nil, nil
+	}
+
+	res := make([]events.Event, 0, len(updates))
+
+	for _, u := range updates {
+		res = append(res, event(u))
+	}
+	p.offset = updates[len(updates)-1].ID + 1
+	return res, nil
+}
+
+func event(upd telegram.Update) events.Event {
+	updType := fetchType(upd)
+	res := events.Event{
+		Type: updType,
+		Text: fetchText(upd),
+	}
+	if updType == events.Message {
+		res.Meta = Meta{
+			ChatId:   upd.Message.Chat.ID,
+			Username: upd.Message.From.Username,
+		}
+	}
+
+	return res
+}
+
+func fetchText(upd telegram.Update) string {
+	if upd.Message == nil {
+		return ""
+	}
+	return upd.Message.Text
+}
+
+func fetchType(upd telegram.Update) events.Type {
+	if upd.Message == nil {
+		return events.Unknown
+	}
+	return events.Message
+}
